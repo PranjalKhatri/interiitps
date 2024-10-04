b83cbc29ec04367eb0956f8e3af8ba135452087f
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2024-10-04 19:11:02 +0530

    basic integration of gemini

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const OpenAI = require("openai");
 const logger = require("./utils/logger");
 require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Initialize Express app
 const app = express();
@@ -22,9 +23,19 @@ app.post("/chat", async (req, res) => {
 app.get("/stream", async (req, res) => {
   // TODO: Stream the response back to the client
 });
+async function getprompt() {
+  // import { GoogleGenerativeAI } from "@google/generative-ai";
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+  const prompt = "Write a story about a magic backpack.";
+
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+}
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   logger.info(`server is running on port ${PORT}`);
+  getprompt();
 });
