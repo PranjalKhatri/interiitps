83370c119a709a69069c9ab8117afcb5b36c048e
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2024-10-04 19:54:40 +0530

    separated prompt logic

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const OpenAI = require("openai");
 const logger = require("./utils/logger");
+const getresponse = require("./prompthandler");
 require("dotenv").config();
-const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Initialize Express app
 const app = express();
@@ -23,19 +23,12 @@ app.post("/chat", async (req, res) => {
 app.get("/stream", async (req, res) => {
   // TODO: Stream the response back to the client
 });
-async function getprompt() {
-  // import { GoogleGenerativeAI } from "@google/generative-ai";
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  const prompt = "Write a story about a magic backpack.";
-
-  const result = await model.generateContent(prompt);
-  console.log(result.response.text());
-}
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   logger.info(`server is running on port ${PORT}`);
-  getprompt();
+  getresponse("Hello").then((val)=>{
+    console.log(val);
+  })
 });
