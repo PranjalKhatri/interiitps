4445293e27386793a9eb27def99113d8818fe0b4
DhruvPansuriya <dhruvpansuriya05@gmail.com>
2024-10-05 11:20:09 +0530

    mongodb done with multi turn chat

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,4 +1,4 @@
-const { text } = require("body-parser");
+// const { text } = require("body-parser");
 const geminiModel = require("../config/genaimodel.js");
 const Chat = require('../models/chat.js');
 
@@ -65,12 +65,6 @@ const chat = async (req, res) => {
     },
 });
 
-<<<<<<< HEAD
-=======
-const { getResponseText, getResponse } = require("../promptHandler");
-
-const chat = async (req, res) => {
->>>>>>> ea7e05b612ce9a67bbfa1567498f77590a0d8603
 
   try {
     console.log("User: ", prompt);
@@ -110,71 +104,6 @@ const getAllChats = async (req,res) => {
   res.json(chats);
 }
 
-<<<<<<< HEAD
-=======
-  // Set headers for streaming response
-  // res.setHeader('Content-Type', 'text/event-stream');
-  // res.setHeader('Cache-Control', 'no-cache');
-  // res.setHeader('Connection', 'keep-alive');
-
-  // Start getting the response
-  // getResponseText(prompt).then(()=>{res.end();});
-  // const ans = getMultiResponse(prompt);
-
-  // ans.then((val) => {
-  //   console.log(val);
-  //   res.json({
-  //     message: val,
-  //   });
-  // }).catch((error) => { 
-  //   // console.log("Error:", error); 
-  //   res.status(500).json({
-  //     message: "Some error has occurred!",
-  //   });
-  // });
-
-  const getChat = async (req, res) => {
-    try {
-        const userID = req.params.userID;
-        const chat = await Chat.findOne({ userId: userID });
-        if (chat) {
-            const { role, parts } = chat; 
-
-            const history = {
-                role: role,
-                parts: parts,
-            };
-
-            console.log(history);
-
-            return res.json({ history });
-        }
-
-        return res.json({
-            message: "Chat not found",
-        });
-    } catch (error) {
-        return res.status(500).json({
-            message: error,
-        });
-    }
-};
-
-//   res.setHeader("Content-Type", "text/plain");
-//   res.setHeader("Cache-Control", "no-cache");
-//   res.setHeader("Connection", "keep-alive");
-
-//   // Start getting the response
-//   getResponse(prompt, res).then(() => {
-//     res.end();
-//   });
-  // const text = getResponseText(prompt);
-  // text.then((val)=>{console.log(val)})
-  // End the response after the stream is complete
-  // res.json({"success":"True"});
-// };
-
->>>>>>> ea7e05b612ce9a67bbfa1567498f77590a0d8603
 module.exports = {
   chat,getAllChats
 };
