30eeb3f067749bb0c529fe79dea0bd84e859bd58
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2024-10-07 19:45:29 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,9 @@ import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
-
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
